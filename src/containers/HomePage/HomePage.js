29320041dc8e2eb95930431aab2c7e2e0b79e24d
Dóra László <design.doralaszlo@gmail.com>
2018-06-28 18:52:48 +0200

    added redux store

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -4,12 +4,27 @@ import React, { Component } from 'react';
 class HomePage extends Component {
   constructor(props) {
     super(props);
+    
+    console.log(props);
+    setTimeout(() => {
+      props.history.push('/login');
+    }, 3000)
   }
 
+  handleClick() {
+    this.props.history.push('/');
+  }
+
+
   render() {
-    <div className="page page-home">
-      <h1>HomePage</h1>
-    </div>
+    return(
+      <div className="page page-home">
+        <h1>HomePage!!!</h1>
+          <button onClick={this.handleClick}>
+            Login
+          </button>
+      </div>
+    );
   }
 }
 
